b4a960364557bf814f2a6a2ef875d1fbfa27e122
Roberto Costa <costamail@gmail.com>
2021-02-19 15:19:15 +0100

    Update coins_list.py

--- coins_list.py
+++ coins_list.py
@@ -14,7 +14,7 @@ import time
 
 coins_list=cg.get_coins_list()
 
-with open('/home/pippo/python/pycoingecko/table_coins_list.csv','wt') as fh:
+with open('table_coins_list.csv','wt') as fh:
 	csvobj=csv.DictWriter(fh,fieldnames=list(coins_list[0].keys()))
 	csvobj.writeheader()
 	csvobj.writerows(coins_list)
